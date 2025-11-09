8b3cc584907cc8f49b9d657bb580536f8d5b2580
Schalexx <peepsaimyt@gmail.com>
2025-11-09 21:39:17 +0100

    Fix: Remove react-helmet-async for React 19 compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 import ProductPage from './ProductPage.jsx'
@@ -12,17 +11,15 @@ import ThankYouPage from './ThankYouPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/what-is-nba-2k26-zen-script" element={<WhatIsPage />} />
-          <Route path="/faq" element={<FAQPage />} />
-          <Route path="/setup-guide" element={<SetupGuidePage />} />
-          <Route path="/thank-you" element={<ThankYouPage />} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/what-is-nba-2k26-zen-script" element={<WhatIsPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/setup-guide" element={<SetupGuidePage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
